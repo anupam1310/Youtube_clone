e0d8423eea7796be376b8ed798f3cbfccb2188c2
anupam1310 <65774208+anupam1310@users.noreply.github.com>
2025-09-30 20:48:13 +0530

    Made changes to back end to start building login and register page

--- Youtube_server/Controller/User.Controller.js
+++ Youtube_server/Controller/User.Controller.js
@@ -6,6 +6,11 @@ import bcrypt from 'bcrypt';
 export async function RegisterUser(req, res) {
     // Registration logic here
     const { username, email, password, avatar_url } = req.body;
+    // Check if user already exists
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+        return res.status(409).json({ message: "User already exists, Login instead" });
+    }
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new UserModel({ username, email, password: hashedPassword, avatar_url });
@@ -23,16 +28,16 @@ export async function LoginUser(req, res) {
     try {
         const user = await UserModel.findOne({ email });
         if (!user) {
-            return res.status(404).json({ message: "User not found" });
+            return res.status(400).json({ message: "User not found, check your email" });
         }
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(401).json({ message: "Invalid credentials" });
+            return res.status(400).json({ message: "Invalid credentials, check your password" });
         }
         const token = jwt.sign({ userId: user._id }, "SecretKey", { expiresIn: "1h" });
         res.status(200).json({ message: "Login successful", token });
     } catch (error) {
-        res.status(500).json({ message: "Error logging in" });
+        res.status(500).json({ message: "Error logging in", error });
         console.log(error);
     }
 }
--- Youtube_server/Server.js
+++ Youtube_server/Server.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 import UserRouter from "./Router/User.router.js";
 import VideoRouter from "./Router/Video.router.js";
 import ChannelRouter from "./Router/Channel.router.js";
+import cors from "cors";
 
-
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 const app = express();
 const PORT = 4050;
@@ -13,7 +16,6 @@ app.listen(PORT, () => {
   // Server is running
   console.log(`Server is listening on port ${PORT}`);
 });
-app.use(express.json());
 
 mongoose.connect('mongodb+srv://anupamkaushik3_db_user:OC3cHmokdFEDACcz@server.wvri3ec.mongodb.net/').
 then(() => {
